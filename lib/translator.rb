e079c12861b05d5ccd03a2721ac9ecc11b5fbc53
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-30 04:19:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def load_library(path)
   new_emoticons = emoticons.each_with_object({}) do |(key, val), hash|
     hash[key => val[0]] = val[1]
   end
-emoticon_pairs = emoticons.each_with_object({}) do |(phrase, symbols), meaning|
+emoticon_pairs = emoticons.each_with_object({}) do |(phrase, symbols), emotes|
     symbols.each_with_index do |english, index|
       if index == 0 
         meaning[english]=phrase
