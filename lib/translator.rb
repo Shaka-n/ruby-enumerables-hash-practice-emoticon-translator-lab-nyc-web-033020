3a7501ee597c2acb63e9c4f877c63c4a214eae67
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-30 04:17:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path)
   pp emoticons
   meaning = {}
   emoticon = {}
-  meaning = emoticons.each_with_object({}) do |(phrase, symbols), meaning|
+  meaning_emoticon_pairs = emoticons.each_with_object({}) do |(phrase, symbols), meaning|
     symbols.each_with_index do |japanese, index|
       index == 1 ? meaning[japanese]=phrase : nil
       puts meaning
@@ -15,21 +15,15 @@ def load_library(path)
   new_emoticons = emoticons.each_with_object({}) do |(key, val), hash|
     hash[key => val[0]] = val[1]
   end
-#emoticon = emoticons.each_with_object({}) do |(phrase, symbols), meaning|
- #   symbols.each_with_index do |english, index|
- #     if index == 0 
-#        meaning[english]=phrase
- #     end
- #   end
-#  end
-#  puts "FINAL EMOTICON"
-#final_emoticon = emoticon.each_with_index({}) do |(key, index) bundled_emoticons|
-#  bundled_emoticons[emoticon][key]= meaning[index]
-#  puts "FINAL EMOTICON"
-#  puts final emoticon
-#end
+emoticon_pairs = emoticons.each_with_object({}) do |(phrase, symbols), meaning|
+    symbols.each_with_index do |english, index|
+      if index == 0 
+        meaning[english]=phrase
+      end
+    end
+  end
 
-library = {:get_meaning => meaning, :get_emoticon =>new_emoticons}
+final_hash = {:get_meaning => meaning_emoticon_pairs, :get_emoticon =>emoticon_pairs}
 end
 
 def get_japanese_emoticon
